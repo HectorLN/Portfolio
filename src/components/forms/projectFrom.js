62c8543a1f57713cc3d2af1b6bdb3eba8212eb67
hectorln <hector.lucero193@tectijuana.edu.mx>
2023-05-20 14:05:39 -0700

    5/11 - Admin started

---
+++ src/components/forms/projectFrom.js
@@ -0,0 +1,13 @@
+import { Grid } from "@mui/material";
+
+export default function ProjectForm() {
+  return (
+    <form>
+        <Grid container spacing={4}>
+            <Grid item xs={12} sm={6}>
+                
+            </Grid>
+        </Grid>
+    </form>
+  )
+}
---
+++ src/components/modals/addProject_Modal.js
@@ -0,0 +1,12 @@
+import { Dialog, DialogContent, DialogTitle } from "@mui/material";
+
+export default function AddProject_Modal() {
+  return (
+    <Dialog>
+        <DialogTitle>Adding a new Project</DialogTitle>
+        <DialogContent>
+            
+        </DialogContent>
+    </Dialog>
+  )
+}
