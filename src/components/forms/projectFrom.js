f93b08672c2e028637d8a630c3178496d73f6e52
hectorln <hector.lucero193@tectijuana.edu.mx>
2023-05-20 14:07:11 -0700

    Added note

--- src/components/forms/projectFrom.js
+++ src/components/forms/projectFrom.js
@@ -1,11 +1,11 @@
 import { Grid } from "@mui/material";
-
+//min 29 may 11 video
 export default function ProjectForm() {
   return (
     <form>
         <Grid container spacing={4}>
             <Grid item xs={12} sm={6}>
-                
+
             </Grid>
         </Grid>
     </form>
