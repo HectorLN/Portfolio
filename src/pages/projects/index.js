b5dfd4f2c82eff082744fc0c2083af581011947b
Hector Lucero <hector.lucero193@tectijuana.edu.mx>
2023-05-24 15:40:30 -0700

    5/12 Inicio

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,5 +1,5 @@
 import Page_template from "@/components/Page_template";
-import ProjectItem from "@/components/ProjectItem";
+import ProjectItem from "@/components/ProjectItem/Index";
 
 export default function ProjectsPage({projects}) 
 {
